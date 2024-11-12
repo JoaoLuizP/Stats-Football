0e4cbb3586a6694c833f569e6c40af8720faa475
Joao Luiz <jlpad06@hotmail.com>
2024-11-12 13:43:22 -0300

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -57,7 +57,7 @@ jogos_do_dia_page = st.Page(
 
 pontos_x_realidade = st.Page(
     page="pages/9_Pontos x realidade.py",
-    title="Expectativa x Realidade",
+    title="Expectativa vs Realidade",
     icon=":material/stacked_line_chart:"
 )
 
